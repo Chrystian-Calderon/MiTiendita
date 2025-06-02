529489993521cfa0f4b96f6efe86eb10e40fc9af
ralf <chrystianralfc@gmail.com>
2025-06-02 12:48:39 -0400

    prueba

--- server/server.js
+++ server/server.js
@@ -20,9 +20,9 @@ const app = express();
 app.use(cors(
   {
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-    credentials: true,
   }
 ));
+console.log(process.env.FRONTEND_URL)
 app.use(express.json());
 const PORT = process.env.PORT;
 
