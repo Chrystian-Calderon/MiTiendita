83667cc560ed19f85112b81aa8a8688f163b8916
ralf <chrystianralfc@gmail.com>
2025-06-04 16:14:14 -0400

    feat: update component navbar with styles and update styles of table

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,26 +1,34 @@
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
+import { BarChart3, LogOut, Menu, Package, PieChart, Settings, Users, ShoppingCart, TrendingUp, Utensils } from "lucide-react"
 
 const Navigation = () => {
+  const navigate = useNavigate()
+  const handleLogout = () => {
+    localStorage.removeItem('auth')
+    navigate('/')
+  }
+
   return (
     <nav className='navbar'>
       <h1>Mi Tiendita</h1>
       <ul>
         <li>
-          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/dashboard"><BarChart3 />Dashboard</Link>
         </li>
         <li>
-          <Link to="/products">Productos</Link>
+          <Link to="/products"><Package />Productos</Link>
         </li>
         <li>
-          <Link to="/shopping">Compras</Link>
+          <Link to="/shopping"><TrendingUp />Compras</Link>
         </li>
         <li>
-          <Link to="/dishes">Platos</Link>
+          <Link to="/dishes"><Utensils />Platos</Link>
         </li>
         <li>
-          <Link to="/sales">Ventas</Link>
+          <Link to="/sales"><ShoppingCart />Ventas</Link>
         </li>
       </ul>
+      <button onClick={handleLogout}><LogOut />Cerrar Sesión</button>
     </nav>
   )
 }
